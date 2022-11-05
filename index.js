42e40baddd72b93ca1d8e263e71ed8b514e0661e
Emma Velazquez <emmasiren24@gmail.com>
2022-11-05 00:59:21 -0700

    Wrote question prompts.

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const md = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for your project?',
+        name: 'installation', 
+    },
+    {
+        type: 'input',
+        message: 'What information is needed for usage?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines for your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What the instructions for testing?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'What license is your project covered under?',
+        name: 'license',
+        choices: ['one', 'two', 'three'],
+    },
+    {
+        type: 'input', 
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
